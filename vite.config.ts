1512d4b17c4b6acc96542fa0cc51e0bda8d3cac6
Reen <renzo.pm99@gmail.com>
2026-03-31 02:24:18 -0300

    add custom domain cname

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import path from "node:path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: '/',
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-});
+})
